a71cdd45df540518cac5b9f600dcbd056b09f3a7
takatama <takatama@users.noreply.github.com>
2026-05-01 19:50:19 +0900

    Update background music asset

--- src/ReplicatedStorage/Shared/Config.lua
+++ src/ReplicatedStorage/Shared/Config.lua
@@ -12,7 +12,7 @@ local Config = {
 	TargetClickSoundId = "rbxassetid://1517589139",
 	TargetHitSoundId = "rbxassetid://74769245504800",
 	StageClearCheerSoundId = "rbxassetid://7755719721",
-	BackgroundMusicSoundId = "rbxassetid://116708421723180",
+	BackgroundMusicSoundId = "rbxassetid://90950951525842",
 	BackgroundMusicVolume = 0.25,
 
 	CrosshairColor = Color3.fromRGB(255, 255, 255),
